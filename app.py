361e5e91c871e0786ac1968d5624c7fb3bbfb768
Jyoti303 <jtripathy393@gmail.com>
2025-05-04 21:41:17 +0530

    added script

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask App on AWS!"
+    return "Hello from Flask App on AWS!Admire You"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
